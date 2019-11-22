d8bbeae0c67f33ea2c346e42547620be498ac65c
madprops <easywest@protonmail.com>
2019-11-22 11:03:48 -0600

    Config path update and install files renames

--- src/dbase.nim
+++ src/dbase.nim
@@ -23,13 +23,13 @@ proc get_db_path(): string =
   if conf.dev:
     getCurrentDir().parentDir().joinPath("db.json")
   else:
-    getHomeDir().joinPath(".config/sym/db.json")
+    getConfigDir().joinPath("sym/db.json")
 
 proc get_db_backup_path(): string =
   if conf.dev:
     getCurrentDir().parentDir().joinPath("db_backup.json")
   else:
-    getHomeDir().joinPath(".config/sym/db_backup.json")
+    getConfigDir().joinPath("sym/db_backup.json")
 
 proc read_db_file(): string =
   try:
